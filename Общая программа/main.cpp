eff43b2aab0cd992298961d6b3c11cac8d09b359
BlueMadWolf <sasha_kulishova@mail.ru>
2017-04-06 21:34:42 +0300

    указано более точное время

--- Общая программа/main.cpp
+++ Общая программа/main.cpp
@@ -47,7 +47,7 @@ int main()
 		auto csz = Center_and_Size(frame, minR, maxR, minG, maxG, minB, maxB, 200, (frame.cols + frame.rows) / 2.0, 11.0);
 		alSource3f(sourceid, AL_POSITION, csz[0] / 100, csz[2] / 100, csz[1] / 100);
 		Sleep(50);
-		if ((double(clock() - start) / CLOCKS_PER_SEC) > (t - 0.5))
+		if ((double(clock() - start) / CLOCKS_PER_SEC) > (t - 0.1))
 			break;
 	}
 	return 0;
